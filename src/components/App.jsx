9c02fd86bcf5c0be70316b59ea91a329e6f601f9
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-06-29 17:57:30 -0400

    Implement state and effect to rerender the deck in shuffled order each time

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,31 @@
-//import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Card from '../components/Card.jsx'
 import Game from '../api/game.js'
 
 function App() {
 
-  const game = new Game
-  game.start()
-  //game.debug()
+  const [game] = useState(() => {
+    const newGame = new Game();
+    newGame.start();
+    newGame.debug();
+    return newGame;
+  });
+  const [deck, setDeck] = useState([...game.deck]);
+
+  useEffect(() => {
+    setDeck([...game.deck]); // Initialize deck state
+  }, [game]);
 
   const handleCardClick = (index) => {
-    game.select(index)
-    game.shuffle()
-    game.debug()
-  }
-
-  // 
+    game.select(index);
+    game.shuffle();
+    setDeck([...game.deck]); // Update state to trigger re-render
+    game.debug();
+  };
 
   return (
     <>
-      {game.deck.slice(0, game.deck.length).map((card) => (
+      {deck.slice(0, game.deck.length).map((card) => (
         <Card index={card.index} key={card.index} onClick={handleCardClick} />
       ))}
     </>
